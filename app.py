7233ebc098d52cd39cf51bd1c77f3aa03d6b3d12
Kle012 <kimhle.0102@gmail.com>
2022-11-23 18:23:32 -0800

    Change methods

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, request, session, jsonify, flash, redirect
-from flask_debugtoolbar import DebugToolbarExtension
+from flask import Flask, render_template, request, session, jsonify
+# from flask_debugtoolbar import DebugToolbarExtension
 from boggle import Boggle
 
 app = Flask (__name__)
 app.config['SECRET_KEY'] = "secret"
-debug = DebugToolbarExtension(app)
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+# debug = DebugToolbarExtension(app)
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
  
 
 boggle_game = Boggle()
@@ -15,35 +15,30 @@ def home_page():
     """Displaying the Board"""
     board = boggle_game.make_board()
     session["board"] = board
-    guesses = session.get("guesses", 0)
+    plays = session.get("plays", 0)
     highscore = session.get("highscore", 0)
-    return render_template("home.html", board=board, guesses=guesses, highscore=highscore)
+    return render_template("home.html", board=board, plays=plays, highscore=highscore)
 
 
-@app.route("/check-word", methods=["POST"])
+@app.route("/check-word")
 def check_word():
     """Check for a Valid Word"""
-    word = request.form.get("word", False)
+    word = request.args["word"]
     board = session["board"]
-
-    if (word):
-        res = boggle_game.check_valid_word(board, word)
-        return jsonify({"result": res})
-    else:
-        flash("No Answer Received!", "error")
-        return redirect("/")
+    res = boggle_game.check_valid_word(board, word)
+    return jsonify({"result": res})
 
 @app.route("/keep-score", methods=["POST"])
 def keep_score():
-    """Posting a Score"""
-    score = request.json["score"]
-    # import pdb
-    # pdb.set_trace()
+    """Posting a Score: get score, update highscore and guesses """
+    data = request.get_json()
+    score = data["score"]
     highscore = session.get("highscore", 0)
-    guesses = session.get("guesses", 0)
+    plays = session.get("plays", 0)
 
+    session["plays"] = plays + 1
     session["highscore"] = max(score, highscore)
-    session["guesses"] = guesses + 1
+    
 
     return jsonify(highestScore = score > highscore)
 
